12bff686e32aeb12471cb84fec128313c67ba6c3
Kinga Jutrzenka Trzebiatowska <jutrzenkatkinga@gmail.com>
2026-05-13 17:14:16 +0200

    Add files via upload

--- kolokwium/k1.sql
+++ kolokwium/k1.sql
@@ -1,48 +1,63 @@
-SELECT * 
-FROM tweets
-ORDER BY tweet_id;
+--7
 
---1 
-
-SELECT tweet_id
-FROM tweets
-GROUP BY tweet_id
-HAVING LENGTH(text) > 250;
-
---2
-
-SELECT text, likes
-FROM tweets
-WHERE likes > 4
-ORDER BY likes;
-
---3
+SELECT * FROM tweets;
 
 SELECT * FROM users;
 
-SELECT login, TRIM(TO_CHAR(registered, 'Day'))
-FROM users
-WHERE TRIM(TO_CHAR(registered, 'Day')) NOT IN('Saturday', 'Sunday');
+SELECT DISTINCT login
+FROM users AS u
+INNER JOIN tweets AS t
+ON u.user_id = t.posted_by
+WHERE registered = posted_date
+ORDER BY login;
 
---4 
+--8
 
-SELECT COUNT(login), TRIM(TO_CHAR(registered, 'Day'))
-FROM users
-GROUP BY TRIM(TO_CHAR(registered, 'Day'))
-HAVING COUNT(login) > 15;
+SELECT likes
+FROM tweets
+ORDER BY likes DESC;
 
---5
+SELECT DISTINCT login, likes 
+FROM tweets AS t
+INNER JOIN users AS u
+ON u.user_id = t.posted_by
+WHERE likes = (SELECT MAX(likes) FROM tweets);
+
+--9
+
+SELECT views
+FROM tweets
+ORDER BY views DESC;
+
+SELECT  DISTINCT login, views 
+FROM tweets AS t
+INNER JOIN users AS u
+ON u.user_id = t.posted_by
+WHERE views = (SELECT MAX(views) FROM tweets);
+
+--10
+
+SELECT * FROM likes;
+SELECT * FROM users;
+SELECT * FROM tweets;
+
+SELECT login, COUNT(DISTINCT l.user_id)
+FROM users AS u
+INNER JOIN tweets AS t
+ON u.user_id = t.posted_by
+INNER JOIN likes AS l
+ON t.tweet_id = l.likes_post
+GROUP BY login;
+
+--11
 
 SELECT * FROM follows;
+SELECT * FROM users;
 
-SELECT COUNT(follows_id), user_id
-FROM follows
-GROUP BY user_id
-ORDER BY COUNT(follows_id);
+SELECT login
+FROM users AS u
+INNER JOIN follows AS f
+ON u.user_id = f.follows_id
+WHERE f.user_id = (SELECT user_id FROM users WHERE login = '@ljaskowicz2o');
 
---6
-
-SELECT COUNT(user_id), follows_id
-FROM follows
-GROUP BY follows_id
-ORDER BY COUNT(user_id);
+--12 analogiczne
